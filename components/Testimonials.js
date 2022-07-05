9f3a2ff5036ec633e5cda93a7386df9ab3a39359
MarioG8 <golikmariusz7@gmail.com>
2022-07-05 12:44:49 +0200

    cleaned up the project unnecessary files and fix the content

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -11,39 +11,39 @@ function Testimonials() {
       <TestimonialsWrapper>
         <Testimonial
           img="/images/runner_000.jpg"
-          name="Mario Golik"
+          name="Eduardo Silva"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
-          desc="marathoner"
+          desc="Ultra-marathoner"
         />
         <Testimonial
           img="/images/runner_001.jpg"
-          name="Mario Golik"
+          name="Camilla Morales"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
           desc="marathoner"
         />
         <Testimonial
           img="/images/runner_002.jpg"
-          name="Mario Golik"
+          name="Barton Moore"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
           desc="marathoner"
         />
         <Testimonial
           img="/images/runner_003.jpg"
-          name="Mario Golik"
+          name="Monica Mendez"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
-          desc="marathoner"
+          desc="Half-marathoner"
         />
         <Testimonial
           img="/images/runner_004.jpg"
-          name="Mario Golik"
+          name="Victor Alamo"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
           desc="marathoner"
         />
         <Testimonial
           img="/images/runner_005.jpg"
-          name="Mario Golik"
+          name="Stefan Szwarz"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
-          desc="marathoner"
+          desc="Ultra-marathoner"
         />
       </TestimonialsWrapper>
     </>
